bb8d1adfd29fe2783333adabe19cb7af2e2c5113
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2021-04-16 22:12:54 +0100

    added new files for training

--- src/test.py
+++ src/test.py
@@ -10,8 +10,8 @@ except ImportError:
 
 lang = sys.argv[1]
 
-img_path = '/app/data/validation/0.tif'
+img_path = '/app/data/validation/0.png'
 img = Image.open(img_path)
 raw_text = pytesseract.image_to_string(img, lang=lang, config='--psm 7')  # make sure to change your `config` if different 
-target = "The computers are becoming sentient,"
+target = "1 480 516 534 154 405 445 511 580 268"
 print(f"Output: {raw_text}\nPercent coincidence: {round(SQ(None, target, raw_text).ratio()*100,2)}%")
