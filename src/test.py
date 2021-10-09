d7d2637c28c939bfc54d6e22e5aa529474118fdf
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2021-10-08 20:59:06 -0400

    prep for pr

--- src/test.py
+++ src/test.py
@@ -10,8 +10,8 @@ except ImportError:
 
 lang = sys.argv[1]
 
-img_path = '/home/ubuntu/train-tesseract/data/validation/0.png'
+img_path = '/app/data/validation/0.tif'
 img = Image.open(img_path)
-raw_text = pytesseract.image_to_string(img, lang=lang, config='--psm 6 -c tessedit_char_whitelist=1234567890-.')  # make sure to change your `config` if different 
-target = "1 480 516 534 154 405 445 511 580 268"
+raw_text = pytesseract.image_to_string(img, lang=lang, config='--psm 7')  # make sure to change your `config` if different 
+target = "The computers are becoming sentient,"
 print(f"Output: {raw_text}\nPercent coincidence: {round(SQ(None, target, raw_text).ratio()*100,2)}%")
